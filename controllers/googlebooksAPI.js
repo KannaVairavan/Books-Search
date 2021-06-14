c523c5d0dee78024bfc7e5e521f42983579aacd4
Kannathal Vairavan <kannamanickam@gmail.com>
2021-06-13 19:10:02 -0500

    Added API.JS fetch call mongoDB and google books API

---
+++ controllers/googlebooksAPI.js
@@ -0,0 +1,21 @@
+// AIzaSyAbwQTUX0QNtL9MjXlFd8VBnYTF76MRxZU
+
+const axios =require('axios');
+
+
+module.exports = {
+  bookSearch: function(req, res) {
+    
+
+    const booksresult = axios.get(`https://www.googleapis.com/books/v1/volumes?q=" + ${bookTitle}`)
+    .then(resp => {
+      return({restaurant: resp.data.results.slice(0,6)});
+    })
+   
+    Promise.all([booksresult]).then((values) => {
+      console.log(values);
+      res.send({...values[0]});
+    });
+    
+  }
+};
